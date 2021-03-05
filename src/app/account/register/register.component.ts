021fc45e6d3a548ce1ca1906c9fff0fbf8c825d2
Miller Juma <miller.developer.william@gmail.com>
2021-03-05 16:06:00 +0300

    validations

--- src/app/account/register/register.component.ts
+++ src/app/account/register/register.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {FormGroup, FormBuilder, Validators} from '@angular/forms';
+import {FormGroup, FormBuilder, Validators, FormControl, Form} from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -9,17 +9,44 @@ import {FormGroup, FormBuilder, Validators} from '@angular/forms';
 export class RegisterComponent{
 
   registrationForm: FormGroup = this.fb.group({
-    firstName : ['', Validators.required],
-    lastName  : ['', Validators.required],
-    username  : ['', Validators.required],
-    email     : ['', Validators.required, Validators.email],
-    phone     : ['', Validators.required],
-    password  : ['', Validators.required],
+    firstName : ['', [Validators.required, Validators.minLength(4)]],
+    lastName  : ['', [Validators.required, Validators.minLength(4)]],
+    username  : ['', [Validators.required, Validators.minLength(4)]],
+    email     : ['', [Validators.required, Validators.email, Validators.minLength(6) ]],
+    phone     : ['', [Validators.required, Validators.required, Validators.minLength(10)]],
+    password  : ['', [Validators.required, Validators.minLength(8)]],
   });
   constructor(private fb: FormBuilder) { }
 
   onSubmit(): void
   {
     console.log(this.registrationForm.value);
+  }
+
+  get firstName(): FormControl
+  {
+    return this.registrationForm.get('firstName') as FormControl;
+  }
+  get lastName(): FormControl
+  {
+    return this.registrationForm.get('lastName') as FormControl;
+  }
+
+  get username(): FormControl
+  {
+    return this.registrationForm.get('username') as FormControl;
+  }
+  get email(): FormControl
+  {
+    return this.registrationForm.get('email') as FormControl;
+  }
+
+  get phone(): FormControl
+  {
+    return this.registrationForm.get('phone') as FormControl;
+  }
+  get password(): FormControl
+  {
+    return this.registrationForm.get('password') as FormControl;
   }
 }
