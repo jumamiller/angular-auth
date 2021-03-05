b4ade2b2338ec9465eb6cbacdfa0b4b78e0d4520
Miller Juma <miller.developer.william@gmail.com>
2021-03-05 12:51:21 +0300

    register fields

--- src/app/account/register/register.component.ts
+++ src/app/account/register/register.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import {FormGroup, FormBuilder, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
-export class RegisterComponent implements OnInit {
+export class RegisterComponent{
 
-  constructor() { }
+  registrationForm: FormGroup = this.fb.group({
+    firstName : ['', Validators.required],
+    lastName  : ['', Validators.required],
+    username  : ['', Validators.required],
+    email     : ['', Validators.required, Validators.email],
+    phone     : ['', Validators.required],
+    password  : ['', Validators.required],
+  });
+  constructor(private fb: FormBuilder) { }
 
-  ngOnInit(): void {
+  onSubmit(): void
+  {
+    console.log(this.registrationForm.value);
   }
-
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AlertComponent } from './_components/alert/alert.component';
@@ -30,7 +30,9 @@ import {MaterialModule} from './material-module';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MaterialModule
+    MaterialModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
